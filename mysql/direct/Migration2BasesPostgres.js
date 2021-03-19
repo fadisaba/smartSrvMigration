c813097252d0ff514554aee065500010d026af8a
nicolas <fadisaba@imsoftware.Fr>
2021-03-19 12:40:05 +0100

    feat(smartSrvMigration): add migration 2 bases postgres

--- mysql/direct/Migration2BasesPostgres.js
+++ mysql/direct/Migration2BasesPostgres.js
@@ -1,165 +1,89 @@
 let uuid = require('node-uuid');
+let dbUtility = require('../common/DbUtility');
+let moment = require('../node_modules/moment');
 let Migration = {
+    /*
+        migratePatient:async function()
+        {
+            let filtersArray=[{name:"patientMigrationId",value:'A11045572941'}];
+            let patientMigrateArray= await dbUtility.read({limit:"1",filters:filtersArray}, "patient","_migrate");
+            patientMigrateArray[0].patientId=uuid.v4();
+            patientMigrateArray[0].patientBirthday=moment(patientMigrateArray[0].patientBirthday).format('Y-M-D');
+            let patientArrray=dbUtility.insertRecords(patientMigrateArray,"patient",false);
+            console.log("migration patient successful");
 
-    migrateActes:function()
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "acte","_migrate")
-            .then(_rows=>{
-                dbUtility.insertRecords(_rows,"acte",false)
-                    .then(results=>{
-                        console.log("migration actes successful")
-                })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
-    },
-    migrateMutuelles:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "mutuelle","_migrate")
-            .then(_rows=>{
-                _rows.forEach(_row=>{
-                    _row.mutuelleId=uuid.v4();
-                });
-                dbUtility.insertRecords(_rows,"mutuelle",false)
-                    .then(results=>{
-                        console.log("migration mutuelle successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
-    },
-    migrateAmo:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "amo","_migrate")
-            .then(_rows=>{
-                _rows.forEach(_row=>{
-                    _row.amoId=uuid.v4();
-                });
-                dbUtility.insertRecords(_rows,"amo",false)
-                    .then(results=>{
-                        console.log("migration amo successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
+        },*/
+    migrateVisit: async function (_sqlStartDate, _sqlEndDate) {
+        let filtersArray = [{name: "visitMigrationId", compare: 'notnull'}];
+        //    let filtersArray=[];
+        filtersArray.push({name: 'visitDate', value1: _sqlStartDate, value2: _sqlEndDate, compare: 'between'});
+        let visitMigrateArray = await dbUtility.read({limit: "no", filters: filtersArray}, "visit", "_migrate");
+        visitMigrateArray.forEach(function (_item) {
+            _item.visitDate = moment(_item.visitDate).format('Y-M-D');
+        });
+
+        let resultArray = dbUtility.insertRecords(visitMigrateArray, "visit", false);
+        console.log(": migration visit successful");
+
     },
 
-    migrateCcamConfig:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "CCAM_CONFIG","_migrate")
-            .then(_rows=>{
-                dbUtility.insertRecords(_rows,"CCAM_CONFIG",false)
-                    .then(results=>{
-                        console.log("migration CCAM_CONFIG successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
-    },
-    migrateStudyType:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "STUDY_TYPE","_migrate")
-            .then(_rows=>{
-                dbUtility.insertRecords(_rows,"STUDY_TYPE",false)
-                    .then(results=>{
-                        console.log("migration STUDY_TYPE successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
-    },
-    migrateStudy:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "STUDY","_migrate")
-            .then(_rows=>{
+    migrateWorklist: async function (_sqlStartDate, _sqlEndDate) {
+        //let filtersArray=[{name:"visitMigrationId",compare:'notnull'}];
+        let filtersArray = [];
+        filtersArray.push({name: 'visitMigrationDate', value1: _sqlStartDate, value2: _sqlEndDate, compare: 'between'});
+        let worklistMigrateArray = await dbUtility.read({limit: "no", filters: filtersArray}, "worklist", "_migrate");
 
-                dbUtility.insertRecords(_rows,"STUDY",false)
-                    .then(results=>{
-                        console.log("migration STUDY successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
+        let resultArray = dbUtility.insertRecords(worklistMigrateArray, "worklist", false);
+        console.log(": migration worklist successful");
+
     },
 
-    migrateStudyActe:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "STUDY_ACTE","_migrate")
-            .then(_rows=>{
+    migrateReport: async function (_sqlStartDate, _sqlEndDate) {
+        let filtersArray=[{name:"reportMigrationId",compare:'notnull'}];
+        filtersArray.push({name: 'visitMigrationDate', value1: _sqlStartDate, value2: _sqlEndDate, compare: 'between'});
+        let reportMigrateArray = await dbUtility.read({limit: "no", filters: filtersArray}, "report", "_migrate");
 
-                dbUtility.insertRecords(_rows,"STUDY_ACTE",false)
-                    .then(results=>{
-                        console.log("migration STUDY_ACTE successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
+        let resultArray = dbUtility.insertRecords(reportMigrateArray, "report", false);
+        console.log(": migration report successful");
+
     },
 
-    migrateModality:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "MODALITY","_migrate")
-            .then(_rows=>{
+    migrateRego: async function (_sqlStartDate, _sqlEndDate) {
+        //  let filtersArray=[{name:"visitMigrationId",compare:'notnull'}];
+        let filtersArray = [];
+        filtersArray.push({name: 'visitMigrationDate', value1: _sqlStartDate, value2: _sqlEndDate, compare: 'between'});
+        let regoMigrateArray = await dbUtility.read({limit: "no", filters: filtersArray}, "REGO", "_migrate");
 
-                dbUtility.insertRecords(_rows,"MODALITY",false)
-                    .then(results=>{
-                        console.log("migration MODALITY successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
+        let resultArray = dbUtility.insertRecords(regoMigrateArray, "REGO", false);
+        console.log(": migration REGO successful");
+
     },
 
-    migrateDevice:function(callback)
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "DEVICE","_migrate")
-            .then(_rows=>{
-                _rows.forEach(_row=>{
+    migrateVisitBalance: async function (_sqlStartDate, _sqlEndDate) {
+        //  let filtersArray=[{name:"visitMigrationId",compare:'notnull'}];
+        let filtersArray = [];
+        filtersArray.push({name: 'visitMigrationDate', value1: _sqlStartDate, value2: _sqlEndDate, compare: 'between'});
+        let visitBalanceMigrateArray = await dbUtility.read({
+            limit: "no",
+            filters: filtersArray
+        }, "VISIT_BALANCE", "_migrate");
 
-                    if(_row.deviceAgreementDate && _row.deviceAgreementDate=="0000-00-00")
-                        _row.deviceAgreementDate="2010-10-10";
-                    if(_row.deviceInstallationDate && _row.deviceInstallationDate=="0000-00-00 00:00:00")
-                        _row.deviceInstallationDate="2010-10-10 10:10:10";
-                });
-                dbUtility.insertRecords(_rows,"DEVICE",false)
-                    .then(results=>{
-                        console.log("migration DEVICE successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
+        let resultArray = dbUtility.insertRecords(visitBalanceMigrateArray, "VISIT_BALANCE", false);
+        console.log(": migration VISIT_BALANCE successful");
+
     },
-    migrateMedecin:function()
-    {
-        let dbUtility = global.App.DbUtility;
-        dbUtility.read({limit:"no"}, "acte","_migrate")
-            .then(_rows=>{
-                dbUtility.insertRecords(_rows,"acte",false)
-                    .then(results=>{
-                        console.log("migration actes successful")
-                    })
-                    .catch(_err=>{
-                        console.error(_err)
-                    })
-            });
-    },
+
+
+    migrateAllTable: async function (_sqlStartDate, _sqlEndDate) {
+
+        await Migration.migrateVisit(_sqlStartDate, _sqlEndDate);
+        await Migration.migrateWorklist(_sqlStartDate, _sqlEndDate);
+        await Migration.migrateReport(_sqlStartDate, _sqlEndDate);
+        await Migration.migrateRego(_sqlStartDate, _sqlEndDate);
+        await Migration.migrateVisitBalance(_sqlStartDate, _sqlEndDate);
+        console.log(": migration VISIT_BALANCE successful");
+
+    }
+
 };
-module.exports = Migration;
+//Migration.migrateAllTable('2021-02-05', '2021-02-05');
